7296264277343bee87f2810e93afe216a857f3fb
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-10 18:37:30 +0200

    Added container

--- templates/index.php
+++ templates/index.php
@@ -17,41 +17,49 @@
     <![endif]-->
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <form class="form-horizontal" method="post">
+            <fieldset>
 
-<form class="form-horizontal" method="post">
-    <fieldset>
+                <!-- Form Name -->
+                <legend><a href="">Расходы</a></legend>
 
-        <!-- Form Name -->
-        <legend><a href="">Расходы</a></legend>
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="textinput">Имя</label>
 
-        <!-- Text input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="textinput">Имя</label>
-            <div class="col-md-4">
-                <input id="field-name" name="name" type="text" placeholder="продукты" class="form-control input-md">
-                <span class="help-block">Имя или категория расходов</span>
-            </div>
-        </div>
+                    <div class="col-md-4">
+                        <input id="field-name" name="name" type="text" placeholder="продукты"
+                               class="form-control input-md">
+                        <span class="help-block">Имя или категория расходов</span>
+                    </div>
+                </div>
 
-        <!-- Text input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="textinput">Сумма</label>
-            <div class="col-md-4">
-                <input id="field-amount" name="amount" type="text" placeholder="99.70" class="form-control input-md">
-                <span class="help-block">Сумма</span>
-            </div>
-        </div>
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="textinput">Сумма</label>
 
-        <!-- Button -->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="singlebutton"></label>
-            <div class="col-md-4">
-                <button type="submit" id="singlebutton" class="btn btn-success">Сохранить</button>
-            </div>
-        </div>
+                    <div class="col-md-4">
+                        <input id="field-amount" name="amount" type="text" placeholder="99.70"
+                               class="form-control input-md">
+                        <span class="help-block">Сумма</span>
+                    </div>
+                </div>
 
-    </fieldset>
-</form>
+                <!-- Button -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="singlebutton"></label>
+
+                    <div class="col-md-4">
+                        <button type="submit" id="singlebutton" class="btn btn-success">Сохранить</button>
+                    </div>
+                </div>
+
+            </fieldset>
+        </form>
+    </div>
+</div>
 </body>
 </html>
 
