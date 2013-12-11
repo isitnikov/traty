b5fa3d663225d3746f54a09ccff810194536de3e
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-11 10:16:27 +0200

    Added config file

--- pub/index.php
+++ pub/index.php
@@ -1,11 +1,19 @@
 <?php
-$config['base_url'] = '/';
 
 define("APP_ROOT_PATH", realpath(__DIR__ . DIRECTORY_SEPARATOR . '..'));
-define("BASE_URL", $config['base_url']);
 define("APP_TEMPLATES_PATH", APP_ROOT_PATH . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
 define("APP_MODELS_PATH", APP_ROOT_PATH . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR);
+define("APP_CONFIGS_PATH", APP_ROOT_PATH . DIRECTORY_SEPARATOR . 'configs' . DIRECTORY_SEPARATOR);
+
+if (!file_exists(APP_CONFIGS_PATH . 'config.php')) {
+    echo "Create config file";
+    die();
+}
+
+require APP_CONFIGS_PATH . 'config.php';
 require APP_MODELS_PATH  . 'operation.php';
+
+define("BASE_URL", $config['base_url']);
 
 if (!empty($_POST)) {
     $operation = new Operation();
