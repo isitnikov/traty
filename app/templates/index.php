9e801efc25e7b9aabf23d3cc2d3ddfe9c8f0ecfd
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-13 17:59:09 +0200

    Added Report Page

--- app/templates/index.php
+++ app/templates/index.php
@@ -30,12 +30,36 @@
     <![endif]-->
 </head>
 <body>
+
+<header class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">За сегодня: <?php echo $todayAmount ?> грн</a></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="<?php echo BASE_URL ?>">Главная</a></li>
+            <li><a href="<?php echo BASE_URL . '?controller=report&action=view' ?>">Отчеты за неделю</a></li>
+        </ul>
+    </div><!-- /.navbar-collapse -->
+</header>
 <div class="container">
+
+
+
         <form class="form-horizontal" method="post" action="<?php echo BASE_URL . '?controller=operation&action=save'?>">
             <fieldset>
 
                 <!-- Form Name -->
-                <legend><a href="">За сегодня: <?php echo $todayAmount ?> грн</a></legend>
+                <legend>Расходы</legend>
 
                 <!-- Text input-->
                 <div class="form-group">
