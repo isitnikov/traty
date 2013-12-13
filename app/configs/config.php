d8561ab04ebebc5b509db99600cc31ebd85a7bed
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-13 13:09:03 +0200

    Remove config from index

--- app/configs/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config['base_url'] = '/misc/money/pub/';
-$config['db']['host'] = 'localhost';
-$config['db']['db'] = 'money';
-$config['db']['user'] = 'root';
-$config['db']['pass'] = 'root';
