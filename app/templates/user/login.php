e1b9842588a0b4894b0edda3ae320bf490c4e273
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-16 18:22:48 +0200

    Default rememberme

--- app/templates/user/login.php
+++ app/templates/user/login.php
@@ -48,7 +48,8 @@
 </style>
 <div class="container">
     <div class="row">
-        <div class="col-md-5 col-md-offset-7">
+        <div class="col-md-7"><h1 style="color: white; font-weight: bold; font-size: 40px">SPEND.SU</h1></div>
+        <div class="col-md-5">
             <div class="panel panel-default">
                 <div class="panel-heading" style="padding-bottom: 0px">
                     <!-- Nav tabs -->
