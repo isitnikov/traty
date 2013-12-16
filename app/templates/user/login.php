3baf4bdee47b27794a768089317648cc25727950
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-16 15:08:21 +0200

    Tab

--- app/templates/user/login.php
+++ app/templates/user/login.php
@@ -48,7 +48,7 @@
 </style>
 <div class="container">
     <div class="row">
-        <div class="col-md-4 col-md-offset-7">
+        <div class="col-md-5 col-md-offset-7">
             <div class="panel panel-default">
                 <div class="panel-heading" style="padding-bottom: 0px">
                     <!-- Nav tabs -->
@@ -74,48 +74,88 @@
 
                     <!-- Tab panes -->
                     <div class="tab-content">
-                        <div class="tab-pane active" id="home">AAAAAA</div>
-                        <div class="tab-pane" id="profile">...</div>
+                        <div class="tab-pane active" id="home">
+
+
+                            <form class="form-horizontal" role="form" method="post" action="<?= GeneralHelper::getUrl('user', 'auth') ?>">
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-4 control-label">
+                                        Пользователь</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name="username" class="form-control" id="inputEmail3" placeholder="username" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputPassword3" class="col-sm-4 control-label">
+                                        Пароль</label>
+                                    <div class="col-sm-8">
+                                        <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="******" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-sm-offset-3 col-sm-9">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="rememberme"/>
+                                                Запомнить меня
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group last">
+                                    <div class="col-sm-offset-3 col-sm-9">
+                                        <button type="submit" class="btn btn-success btn-sm">
+                                            Войти</button>
+                                    </div>
+                                </div>
+                            </form>
+
+
+                        </div>
+                        <div class="tab-pane" id="profile">
+
+
+                            <form class="form-horizontal" role="form" method="post" action="<?= GeneralHelper::getUrl('user', 'registration') ?>">
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-4 control-label">
+                                        Пользователь</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name="username" class="form-control" id="inputEmail3" placeholder="username" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputPassword3" class="col-sm-4 control-label">
+                                        Пароль</label>
+                                    <div class="col-sm-8">
+                                        <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="******" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-sm-offset-3 col-sm-9">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="rememberme"/>
+                                                Запомнить меня
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group last">
+                                    <div class="col-sm-offset-3 col-sm-9">
+                                        <button type="submit" class="btn btn-success btn-sm">
+                                            Регистрация</button>
+                                    </div>
+                                </div>
+                            </form>
+
+
+                        </div>
                     </div>
 
 
 
 
 
-                    <form class="form-horizontal" role="form" method="post" action="<?= GeneralHelper::getUrl('user', 'auth') ?>">
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-3 control-label">
-                                Имя пользователя</label>
-                            <div class="col-sm-9">
-                                <input type="text" name="username" class="form-control" id="inputEmail3" placeholder="username" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword3" class="col-sm-3 control-label">
-                                Пароль</label>
-                            <div class="col-sm-9">
-                                <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="******" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-9">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="rememberme"/>
-                                        Запомнить меня
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group last">
-                            <div class="col-sm-offset-3 col-sm-9">
-                                <button type="submit" class="btn btn-success btn-sm">
-                                    Войти</button>
-                                <button type="reset" class="btn btn-default btn-sm">
-                                    Сбросить</button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
                 <div class="panel-footer">
                     <a href="http://www.jquery2dotnet.com">Регистрация</a></div>
