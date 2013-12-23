322b2d3240580f335ca2f81f99dbec2236306b47
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-23 16:08:55 +0200

    Added type

--- app/templates/index.php
+++ app/templates/index.php
@@ -5,75 +5,142 @@ require 'header.php';
     <div class="row">
         <div class="col-md-6">
 
+            <ul class="nav nav-pills">
+                <li class="active"><a href="#spend" class="glyphicon glyphicon-minus" data-toggle="tab"> Расходы</a></li>
+                <li><a href="#income" class="glyphicon glyphicon-plus" data-toggle="tab"> Доходы</a></li>
+            </ul>
+            <div class="row">&nbsp;</div>
+
+            <div class="tab-content">
+                <div class="tab-pane active" id="spend">
+                    <form role="form" method="post" action="<?php echo GeneralHelper::getUrl('operation', 'save') ?>" id="login">
+                        <fieldset>
+                            <input type="hidden" name="type" value="1" />
+
+                            <!-- Form Name -->
+                            <legend class="sr-only">Расходы</legend>
+
+                            <!-- Text input-->
+                            <div class="form-group">
+                                <label class="control-label sr-only" for="category">Категория</label>
+
+                                <select id="category" name="name" class="form-control input-lg">
+                                    <option value="Транспорт">Транспорт</option>
+                                    <option value="Продукты">Продукты</option>
+                                    <option value="Одежда, косметика">Одежда, косметика</option>
+                                    <option value="Отдых">Отдых</option>
+                                    <option value="Медицина">Медицина</option>
+                                    <option value="Авто">Авто</option>
+                                    <option value="Жилье">Жилье</option>
+                                    <option value="Еда вне дома">Еда вне дома</option>
+                                    <option value="Обучение">Обучение</option>
+                                    <option value="Работа">Работа</option>
+                                    <option value="Спорт">Спорт</option>
+                                    <option value="Разное">Разное</option>
+                                    <option value="Домашние животные">Питомцы</option>
+                                    <option value="Долги, кредиты">Долги, кредиты</option>
+                                    <option value="Накопления">Накопления</option>
+                                    <option value="Связь">Связь</option>
+                                    <option value="Привычки">Привычки</option>
+                                </select>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                        <label class="control-label sr-only" for="textinput">Сумма</label>
+
+                                        <input id="field-amount" name="amount" type="text" placeholder="Сумма"
+                                               pattern="[0-9]+[.,]?[0-9]*"
+                                               class="form-control input-lg">
+                                    </div>
+                                </div>
+                                <div class="col-xs-6">
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                        <label class="control-label sr-only" for="textinput">Дата</label>
+
+                                        <?php $dateValue = date('d/m/Y') ?>
+                                        <input id="field-date" name="date" type="text"
+                                               placeholder="<?php echo $dateValue ?>"
+                                               class="form-control input-lg" value="<?php echo $dateValue ?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Button -->
+                            <div class="form-group">
+                                <label class="control-label" for="singlebutton"></label>
+
+                                <button type="submit" id="singlebutton" class="btn btn-success btn-block btn-lg">Сохранить
+                                </button>
+                            </div>
+
+                        </fieldset>
+                    </form>
+                </div>
 
 
 
-            <form role="form" method="post"
+                <div class="tab-pane" id="income">
+                    <form role="form" method="post" action="<?php echo GeneralHelper::getUrl('operation', 'save') ?>" id="login">
+                        <fieldset>
+                            <input type="hidden" name="type" value="2" />
+                            <!-- Form Name -->
+                            <legend class="sr-only">Расходы</legend>
 
-                  action="<?php echo BASE_URL . '?controller=operation&action=save' ?>">
-                <fieldset>
+                            <!-- Text input-->
+                            <div class="form-group">
+                                <label class="control-label sr-only" for="category">Категория</label>
 
-                    <!-- Form Name -->
-                    <legend class="sr-only">Расходы</legend>
+                                <select id="category" name="name" class="form-control input-lg">
+                                    <option value="Зарплата">Зарплата</option>
+                                    <option value="Подработки">Подработки</option>
+                                    <option value="Проценты вкладов">Проценты вкладов</option>
+                                    <option value="Аренда">Аренда</option>
+                                </select>
+                            </div>
 
-                    <!-- Text input-->
-                    <div class="form-group">
-                        <label class="control-label sr-only" for="category">Категория</label>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                        <label class="control-label sr-only" for="textinput">Сумма</label>
 
-                        <select id="category" name="name" class="form-control input-lg">
-                            <option value="Транспорт">Транспорт</option>
-                            <option value="Продукты">Продукты</option>
-                            <option value="Одежда, косметика">Одежда, косметика</option>
-                            <option value="Отдых">Отдых</option>
-                            <option value="Медицина">Медицина</option>
-                            <option value="Авто">Авто</option>
-                            <option value="Жилье">Жилье</option>
-                            <option value="Еда вне дома">Еда вне дома</option>
-                            <option value="Обучение">Обучение</option>
-                            <option value="Работа">Работа</option>
-                            <option value="Спорт">Спорт</option>
-                            <option value="Разное">Разное</option>
-                            <option value="Домашние животные">Питомцы</option>
-                            <option value="Долги, кредиты">Долги, кредиты</option>
-                            <option value="Накопления">Накопления</option>
-                            <option value="Связь">Связь</option>
-                            <option value="Привычки">Привычки</option>
-                        </select>
-                    </div>
+                                        <input id="field-amount" name="amount" type="text" placeholder="Сумма"
+                                               pattern="[0-9]+[.,]?[0-9]*"
+                                               class="form-control input-lg">
+                                    </div>
+                                </div>
+                                <div class="col-xs-6">
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                        <label class="control-label sr-only" for="textinput">Дата</label>
 
-                    <div class="row">
-                    <div class="col-xs-6">
-                        <!-- Text input-->
-                        <div class="form-group">
-                            <label class="control-label sr-only" for="textinput">Сумма</label>
+                                        <?php $dateValue = date('d/m/Y') ?>
+                                        <input id="field-date" name="date" type="text"
+                                               placeholder="<?php echo $dateValue ?>"
+                                               class="form-control input-lg" value="<?php echo $dateValue ?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Button -->
+                            <div class="form-group">
+                                <label class="control-label" for="singlebutton"></label>
 
-                            <input id="field-amount" name="amount" type="text" placeholder="Сумма"
-                                   pattern="[0-9]+[.,]?[0-9]*"
-                                   class="form-control input-lg">
-                        </div>
-                    </div>
-                    <div class="col-xs-6">
-                        <!-- Text input-->
-                        <div class="form-group">
-                            <label class="control-label sr-only" for="textinput">Дата</label>
+                                <button type="submit" id="singlebutton" class="btn btn-success btn-block btn-lg">Сохранить
+                                </button>
+                            </div>
 
-                            <?php $dateValue = date('d/m/Y') ?>
-                            <input id="field-date" name="date" type="text"
-                                   placeholder="<?php echo $dateValue ?>"
-                                   class="form-control input-lg" value="<?php echo $dateValue ?>">
-                        </div>
-                    </div>
-                        </div>
-                    <!-- Button -->
-                    <div class="form-group">
-                        <label class="control-label" for="singlebutton"></label>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
 
-                        <button type="submit" id="singlebutton" class="btn btn-success btn-block btn-lg">Сохранить
-                        </button>
-                    </div>
 
-                </fieldset>
-            </form>
+
+
+
         </div>
 
         <div class="col-md-6">
