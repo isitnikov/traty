8737557d2b265a02a187844be73753020ae5d6af
Yaroslav Voronoy <yvoronoy@ebay.com>
2013-12-27 17:39:30 +0200

    Implement Budget

--- app/templates/budget/view.php
+++ app/templates/budget/view.php
@@ -4,36 +4,38 @@ require APP_TEMPLATES_PATH . 'header.php';
 <div class="container">
     <h3 class="text-primary">Бюджет <small>на месяц</small></h3>
 
+    <?php
+    $categories = array('Продукты', 'Транспорт', 'Одежда', 'Авто', 'Отдых', 'Еда вне дома', 'Развлечения');
+    ?>
     <div class="row">
         <div class="col-xs-12">
-            <div class="table-responsive">
-            <table class="table table-condensed">
+            <table class="table">
                 <tr>
                     <th>Категория</th>
-                    <th>Бюджет</th>
-                    <th>Декабрь 2013</th>
                     <th>В среднем</th>
+                    <th>Декабрь 2013</th>
                 </tr>
+                <?php foreach ($categories as $category): ?>
                 <tr>
                     <td>Продукты</td>
-                    <td><input type="text" name="amount" placeholder="00.00" class="form-control" /> </td>
-                    <td><?= GeneralHelper::renderAmount(rand(1000, 3000), Category::TYPE_SPEND)?></td>
-                    <td><?= GeneralHelper::renderAmount(rand(1000, 3000), Category::TYPE_SPEND)?></td>
+                    <td><?= GeneralHelper::renderAmount(rand(1000, 10000), Category::TYPE_SPEND) ?></td>
+                    <td><?= GeneralHelper::renderAmount(rand(1000, 10000), Category::TYPE_SPEND) ?></td>
                 </tr>
-                <tr>
-                    <td>Продукты</td>
-                    <td><input type="text" name="amount" placeholder="00.00" class="form-control" /> </td>
-                    <td><?= GeneralHelper::renderAmount(rand(1000, 3000), Category::TYPE_SPEND)?></td>
-                    <td><?= GeneralHelper::renderAmount(rand(1000, 3000), Category::TYPE_SPEND)?></td>
-                </tr>
-                <tr>
-                    <td>Продукты</td>
-                    <td><input type="text" name="amount" placeholder="00.00" class="form-control" /> </td>
-                    <td><?= GeneralHelper::renderAmount(rand(1000, 3000), Category::TYPE_SPEND)?></td>
-                    <td><?= GeneralHelper::renderAmount(rand(1000, 3000), Category::TYPE_SPEND)?></td>
-                </tr>
+                <?php endforeach ?>
             </table>
-                </div>
+            <table class="table">
+                <tr>
+                    <th>Категория</th>
+                    <th class="text-right">Бюджет</th>
+                </tr>
+
+                <?php foreach ($categories as $category): ?>
+                <tr>
+                    <td><?= $category ?></td>
+                    <td class="text-right"><input type="text" name="amount" placeholder="00.00" class="form-control" /> </td>
+                </tr>
+                <?php endforeach ?>
+            </table>
         </div>
     </div>
 </div>
