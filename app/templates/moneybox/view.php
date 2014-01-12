1f5e31f6167ebd6910daaa147659c615bbddfc43
Yaroslav Voronoy <y.voronoy@gmail.com>
2014-01-12 17:53:43 +0200

    Implement Moneybox

--- app/templates/moneybox/view.php
+++ app/templates/moneybox/view.php
@@ -21,7 +21,28 @@
         <li>Накопленно: <?= GeneralHelper::renderAmount(5000) ?></li>
     </ul>
     <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            <span class="sr-only">60% Complete</span>
+        </div>
+    </div>
+    <div class="row">&nbsp;</div>
+</div>
+<div class="moneybox">
+    <div class="row">
+        <div class="col-xs-6">
+            <h4 class="title">Квартира</h4>
+        </div>
+        <div class="col-xs-6 text-right">
+            <h4 class="final-amount"><?= GeneralHelper::renderAmount(500000) ?></h4>
+        </div>
+    </div>
+    <ul class="properties list-unstyled">
+        <li>Ежемесячная сумма: <?= GeneralHelper::renderAmount(5000) ?></li>
+        <li>Запланированная дата: 01/01/2016</li>
+        <li>Накопленно: <?= GeneralHelper::renderAmount(5000) ?></li>
+    </ul>
+    <div class="progress">
+        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
             <span class="sr-only">60% Complete</span>
         </div>
     </div>
